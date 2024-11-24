0b90b48a0a59cbbf07d22446d1d0c1625a897571
Matheus <szmatheussouza@gmail.com>
2024-11-23 22:40:01 -0300

    correção para rodar scroll mais bonito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const Header = () => {
   return (
@@ -6,12 +7,39 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
         <h1 className="text-2xl font-bold">Matheus Souza</h1>
         <nav>
-        <ul className="flex gap-4">
-          <li><a href="#about" className="text-white hover:text-purpleTheme-dark">Sobre</a></li>
-          <li><a href="#projects" className="text-white hover:text-purpleTheme-dark">Projetos</a></li>
-          <li><a href="#contact" className="text-white hover:text-purpleTheme-dark">Contato</a></li>
-        </ul>
-      </nav>
+          <ul className="flex gap-4">
+            <li>
+              <Link 
+                to="about" 
+                smooth={true} 
+                duration={500} 
+                className="text-white hover:text-purpleTheme-dark cursor-pointer"
+              >
+                Sobre
+              </Link>
+            </li>
+            <li>
+              <Link 
+                to="projects" 
+                smooth={true} 
+                duration={500} 
+                className="text-white hover:text-purpleTheme-dark cursor-pointer"
+              >
+                Projetos
+              </Link>
+            </li>
+            <li>
+              <Link 
+                to="contact" 
+                smooth={true} 
+                duration={500} 
+                className="text-white hover:text-purpleTheme-dark cursor-pointer"
+              >
+                Contato
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
